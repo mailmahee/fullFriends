7b86a42daa3487e8789deae617735436918c4a98
Mohammed Abid <kamranmohammedabid@gmail.com>
2016-10-12 18:14:38 -0700

    Final

--- server.py
+++ server.py
@@ -1,39 +1,46 @@
 import time
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, flash
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
-app.secret_key = 'secret'
 mysql = MySQLConnector(app,'friendsdb')
 
 @app.route('/')
 def index():
     friends = mysql.query_db("SELECT * FROM friends")
     return render_template("index.html",friends=friends)
-@app.route('/add', methods=['POST'])
+@app.route('/friends', methods=['POST'])
 def add():
     print request.form['first_name']
     print request.form['last_name']
     print request.form['occupation']
-    print request.form['email']
-    print request.form['created_at']
-
-def create():
     query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
     data = {'first_name': request.form['first_name'],'last_name':  request.form['last_name'],'occupation': request.form['occupation']}
     mysql.query_db(query, data)
     return redirect('/')
-@app.route('/delete', methods=['POST'])
-def deleteRecord():
-    return render_template('delete.html')
-@app.route('/execdelete')
-def execDelete():
-    mysql.query_db("DELETE FROM friends WHERE id = session['id']")
-    return redirect('/')
-@app.route('/update')
-def updateRecord():
-    return render_template('update.html')
-@app.route('/execupdate')
-def execUpdate():
+
+
+@app.route('/update/edit/', methods=['GET'])
+def update():
+    identity = request.args.get('friend_id')
+    action = request.args.get('action')
+    query = "SELECT * FROM friends WHERE id = :id"
+    data = {'id': identity}
+    friend = mysql.query_db(query, data)
+    return render_template('update.html', friend = friend,action = action)
+
+
+@app.route('/friends/<id>', methods=['POST'])
+def execUpdate(id):
+
+    if request.form['action'] == 'update':
+        query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :friend_id"
+        data = {'first_name': request.form['first_name'], 'last_name':  request.form['last_name'], 'occupation': request.form['occupation'], 'friend_id':id}
+    else:
+        query = "DELETE FROM friends WHERE id = :id"
+        data = {'id':id}
+
+
+    mysql.query_db(query, data)
     return redirect('/')
 
 app.run(debug=True)
